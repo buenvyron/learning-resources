4fe8c9a858b1c3275b9684b2f12fb953b08c66f8
Vyron Joshua <buen.vyron@gmail.com>
2024-09-18 15:58:20 +0000

    UPDATE introduction.js new lines

---
+++ 2-js-basics/introduction.js
@@ -0,0 +1,13 @@
+var hello = 'hello';
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if (true) {
+    let world = "Hello World!";
+    console.log(world);
+    console.log(hello);
+}
+
+const VYRON = "Vyron";
+console.log(VYRON);
